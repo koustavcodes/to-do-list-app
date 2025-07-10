a52be4ba41b811e34f734528dca4067d70c42194
Koustav Sarkar <koustavsarkarcoding@gmail.com>
2025-07-10 12:24:37 +0000

    Change: Delete button to look like dustbin

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ function addItem() {
   const li = document.createElement("li");
   li.innerHTML = `
     <span>${text}</span>
-    <button class="delete" onclick="this.parentElement.remove()">Delete</button>
+    <button class="delete" onclick="this.parentElement.remove()" aria-label="Delete">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 16 16">
+        <path fill="currentColor" d="M6 6.75a.75.75 0 0 1 .75.75v4a.75.75 0 0 1-1.5 0v-4A.75.75 0 0 1 6 6.75Zm4 .75a.75.75 0 0 0-1.5 0v4a.75.75 0 0 0 1.5 0v-4ZM2.75 4.75A.75.75 0 0 1 3.5 4h9a.75.75 0 0 1 0 1.5h-.305l-.638 7.016A2.25 2.25 0 0 1 9.32 14H6.68a2.25 2.25 0 0 1-2.237-1.484L3.805 5.5H3.5a.75.75 0 0 1-.75-.75ZM5.07 12.5a.75.75 0 0 0 .71.5h2.64a.75.75 0 0 0 .71-.5l.62-6.5H4.45l.62 6.5ZM6.25 2a.75.75 0 0 1 .75-.75h2a.75.75 0 0 1 0 1.5h-2A.75.75 0 0 1 6.25 2Z"/>
+      </svg>
+    </button>
   `;
   list.appendChild(li);
   input.value = "";
